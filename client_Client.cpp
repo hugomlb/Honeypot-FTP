311c12ecd82642fc70f680245f58a6b4814ae1a6
hugomlb <hlarrea@fi.uba.ar>
2019-09-30 00:55:00 -0300

    Cliente se cierra correctamente

--- client_Client.cpp
+++ client_Client.cpp
@@ -1,5 +1,6 @@
 #include "client_Client.h"
 #include "client_ServerProxy.h"
+#include "common_SocketPeerException.h"
 #include <iostream>
 
 client_Client::client_Client() {
@@ -8,9 +9,12 @@ client_Client::client_Client() {
 void client_Client::run(const char* hostName, const char* service) {
   client_ServerProxy server(hostName, service);
   std::string command = " ";
-  while (command != "QUIT" ) {
-    getline(std::cin, command);
-    server.executeCommand(command);
+  try {
+    while (command != "QUIT" ) {
+      getline(std::cin, command);
+      server.executeCommand(command);
+    }
+  } catch (common_SocketPeerException &e){
   }
 }
 
